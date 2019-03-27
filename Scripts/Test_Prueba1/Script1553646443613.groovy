6caaff081832c2737cedb134af6f81b2bafd4d67
Avhiz7 <d_antho12@hotmail.com>
2019-03-27 05:21:32 -0400

    Se agregaron screenshot

--- Scripts/Test_Prueba1/Script1553646443613.groovy
+++ Scripts/Test_Prueba1/Script1553646443613.groovy
@@ -8,11 +8,19 @@ import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords
+import com.kms.katalon.core.util.KeywordUtil
+
+//Cargar Data File
+//def data = TestDataFactory.findTestData("Test_Data")
+//int rows = data.getRowNumbers()
+
 
 WebUI.openBrowser('')
 
@@ -23,20 +31,34 @@ WebUI.navigateToUrl('www.bhdleon.com.do')
 
 WebUI.verifyElementPresent(findTestObject('Object Repository/Prueba1/Page_Banco BHD Len/a_Contctanos'), 20)
 
-/*WebUI.takeScreenshot()*/
+WebUI.takeScreenshot()
 
 WebUI.click(findTestObject('Object Repository/Prueba1/Page_Banco BHD Len/a_Contctanos'))
 
+//for(i=1; rows; i++){
+
+//	data.Nombre = data.getValue("Nombre", i)
+//	data.Apellido = data.getValue("Apellido", i)
+//	data.Correo = data.getValue("Correo", i)
+//	data.Mensaje = data.getValue("Mensaje", i)
+
+KeywordUtil.markPassed("Se ingresó al formulario correctamente")
+
+WebUI.takeScreenshot()
 
 WebUI.setText(findTestObject('Object Repository/Prueba1/Page_Banco BHD Len/input_Nombre_Nombre'), Nombre)
+//WebUI.setText(findTestObject('Object Repository/Prueba1/Page_Banco BHD Len/input_Nombre_Nombre'), findTestData("Test_Data").getValue(1,i))
 
 WebUI.click(findTestObject('Object Repository/Prueba1/Page_Banco BHD Len/div_Apellido_col-xs-12 col-sm-12 col-md-12 col-lg-7'))
 
 WebUI.setText(findTestObject('Object Repository/Prueba1/Page_Banco BHD Len/input_Apellido_Apellido'), Apellido)
+//WebUI.setText(findTestObject('Object Repository/Prueba1/Page_Banco BHD Len/input_Apellido_Apellido'), findTestData("Test_Data").getValue(1,i))
+
 
 WebUI.click(findTestObject('Object Repository/Prueba1/Page_Banco BHD Len/div_Email_col-xs-12 col-sm-12 col-md-12 col-lg-7'))
 
 WebUI.setText(findTestObject('Object Repository/Prueba1/Page_Banco BHD Len/input_Email_Email'), Correo)
+
 
 WebUI.takeScreenshot()
 
@@ -44,7 +66,22 @@ WebUI.scrollToElement(findTestObject('Object Repository/Prueba1/Page_Banco BHD L
 
 WebUI.setText(findTestObject('Object Repository/Prueba1/Page_Banco BHD Len/textarea_Escrbe el mensaje_Mensaje'), Mensaje)
 
+KeywordUtil.markPassed("Se completaron los datos del formulario correctamente")
+
 WebUI.takeScreenshot()
 
 WebUI.closeBrowser()
 
+//}
+
+
+/*if (elements.size() > 0)
+{
+//Mark Passed status after this step
+KeywordUtil.markPassed("Element is Present")
+}
+else
+{
+//Mark Failed status after this step
+KeywordUtil.markFailed("Element is not presebt")
+}*/
